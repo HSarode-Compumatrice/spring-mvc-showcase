6fc05714c1c40c529e6bd730e3fe87415590612d
HSarode-Compumatrice <hsarode@edrnet.com>
2018-04-06 15:15:41 +0530

    Update codedeploy.sh

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #CodeDeploy Script which generates project specific WAR
 #
-echo "Build Number is: $BUILD_NUMBER"
-echo "Jenkins Workspace: $WORKSPACE"
+cd $WORKSPACE"
 mvn package
 ls -lthr $WORKSPACE/target
